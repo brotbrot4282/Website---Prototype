84725b8c5d10c532621db741613881d56bc88845
Rafli Anwar <148883804+brotbrot4282@users.noreply.github.com>
2024-10-16 12:05:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home', ['title' => 'Home Page']);
-});
+Route::get('/', 'HomeController@index');
 
 Route::get('/home', function () {
     return view('home', ['title' => 'Home Page']);
